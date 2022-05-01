bddd5983d1e3a2859429198b7ee593bed57b53a0
vaca <wsmslgh@gmail.com>
2022-05-01 02:30:02 -0700

    Initial commit

---
+++ main/fuck1.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <esp_log.h>
+#include "driver/i2c.h"
+#include "gt9xx.h"
+#include "myi2c.h"
+static const char *TAG = "REC_RAW_HTTP";
+
+extern "C"
+{
+void app_main();
+}
+
+
+void app_main(void) {
+    i2c_master_init();
+    GT9xx_Class *a1=new GT9xx_Class();
+    a1->begin(0);
+
+
+    uint16_t x=0;
+    uint16_t y=0;
+    uint8_t z=0;
+
+    int bb=0;
+
+    while (true){
+        vTaskDelay(100);
+        bb=a1->scanPoint();
+        a1->getPoint(x,y,z);
+        ESP_LOGE(TAG, "I2C Timeout  %d  %d  %d",x,y,bb);
+    }
+
+
+}
---
+++ main/myi2c.h
@@ -0,0 +1,33 @@
+//
+// Created by vaca on 5/1/22.
+//
+
+#ifndef FUCK1_MYI2C_H
+#define FUCK1_MYI2C_H
+
+#include "driver/i2c.h"
+
+#define FT5206_VENDID                   (0x11)
+#define FT6206_CHIPID                   (0x06)
+#define FT6236_CHIPID                   (0x36)
+#define FT6236U_CHIPID                  (0x64)
+#define FT5206U_CHIPID                  (0x64)
+#define I2C_MASTER_SCL_IO          22
+#define I2C_MASTER_SDA_IO           21
+#define I2C_MASTER_NUM              0                          /*!< I2C master i2c port number, the number of i2c peripheral interfaces available will depend on the chip */
+#define I2C_MASTER_FREQ_HZ          400000                     /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE   0                          /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_TIMEOUT_MS       1000
+#define SENSOR_ADDR                 0x5D
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+esp_err_t i2c_master_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //FUCK1_MYI2C_H
